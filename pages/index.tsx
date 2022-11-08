93eb777167abdbdb1e4c1214fee4ddf8ae3a9698
Alex <olivesargreek@gmail.com>
2022-11-08 14:36:33 +0300

    feat: step_7.4 добавлен компонент <Button />

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
-import { Htag } from "../components";
+import { Htag, Button } from "../components";
+
 
 export default function Home(): JSX.Element {
   return (
     <>
       <Htag tag='h1'>Working...</Htag>
+      <Button appearance='primary'>Button</Button> 
+      <Button appearance='ghost'>Button</Button> 
     </>
   );
 }
