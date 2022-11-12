8f8976fd140af1c03a407b166e37efde01077aea
Alex <olivesargreek@gmail.com>
2022-11-12 14:58:47 +0300

    feat: step_8.5 добавлен интерактив на компонент <Rating />

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,9 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Htag, Button, P, Rating } from "../components";
 import { withLayout } from '../layout/Layout';
 
 function Home(): JSX.Element {
+  const [rating, setRating] = useState<number>(3);
   return (
     <>
       <Htag tag='h1'>Working...</Htag>
@@ -11,7 +12,7 @@ function Home(): JSX.Element {
       <P size='l'>Компонент P big</P>
       <P>Компонент P middle</P>
       <P size='s'>Компонент P small</P>
-      <Rating rating={4}/>
+      <Rating rating={rating} isEditable setRating={ setRating }/>
     </>
   );
 }
